e36553ece875d73161f8346ba29f7b0668e21a65
Makar Emeliyanov <66727639+Pandamime100hp@users.noreply.github.com>
2024-09-21 13:08:00 +0200

    fix: fix config issue.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
-export const nextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist`.
 }
 
-export const images = {
+const images = {
     unoptimized: true,
 };
+
+export default {
+    ...nextConfig,
+    images
+}
