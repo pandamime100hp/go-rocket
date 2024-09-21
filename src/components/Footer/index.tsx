567a36f9088d6c817844db0906bb4d9a062e8b65
Makar Emeliyanov <66727639+Pandamime100hp@users.noreply.github.com>
2024-09-21 02:00:17 +0200

    refactor(css): cleaned up css files to reposition some items.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,22 +4,33 @@ import React from 'react'
 // CSS
 import './footer.min.css'
 
+// ICON
+import { DiGithubBadge } from "react-icons/di";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer: React.FC = () => {
     const year: number = new Date().getFullYear()
 
     return (
-        <footer>
-            <div>
-                <p>Powered by Croissants</p>
-                <p className='copyright'>Copyright &copy; {year}. All rights reserved.</p>
-            </div>
-            <a
-                href="https://github.com/pandamime100hp/go-rocketp"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                
-            </a>
+        <footer className="container">
+            <p className="footer__content">Powered by Croissants</p>
+            <p className="footer__copyright">Copyright &copy; {year}. All rights reserved.</p>
+            <ul className="footer__socials">
+                <li>
+                    <h1>
+                        <a href="https://github.com/pandamime100hp/go-rocket">
+                            <DiGithubBadge />
+                        </a>
+                    </h1>
+                </li>
+                <li>
+                    <h1>
+                        <a href="https://linkedin.com/in/makarintosh">
+                            <FaLinkedin />    
+                        </a>
+                    </h1>
+                </li>
+            </ul>
         </footer>
     )
 }
